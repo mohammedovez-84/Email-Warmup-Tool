95df17f3db6c4ca65b27b7f7863d8fd52440bf98
aasifdemand <aasifdemand@gmail.com>
2025-10-17 19:35:30 +0530

    feat: fixed microsoft auth

--- backend/routes/microsoftRoutes.js
+++ backend/routes/microsoftRoutes.js
@@ -4,29 +4,38 @@ const { saveMicrosoftUser } = require('../controllers/microsoftController');
 
 const router = express.Router();
 
-router.get('/microsoft', passport.authenticate('microsoft', {
-    prompt: 'select_account',
-    scope: ['openid', 'profile', 'offline_access', 'user.read']
-}));
+router.get('/microsoft', (req, res, next) => {
+    const userId = req.query.userId;
+    if (!userId) return res.status(400).send('Missing userId');
 
+    passport.authenticate('microsoft', {
+        prompt: 'select_account',
+        scope: ['openid', 'profile', 'offline_access', 'user.read'],
+        state: userId, // 👈 goes to callback
+    })(req, res, next);
+});
 
 router.get('/microsoft/callback',
     passport.authenticate('microsoft', {
-        failureRedirect: '/login-failed',
+        failureRedirect: 'http://localhost:5173/dashboard',
         session: false
     }),
     async (req, res) => {
         try {
+
+            // console.log("user: ", req.user);
+
             const refreshToken = req.user?._refreshToken;
             const accessToken = req.user?._accessToken;
             const expiresIn = req.user?._expiresIn;
-            await saveMicrosoftUser(req.user, refreshToken, accessToken, expiresIn);
+            const userId = req.query.state;
+            await saveMicrosoftUser(req.user, refreshToken, accessToken, expiresIn, userId);
 
 
             res.redirect('http://localhost:5173/dashboard');
         } catch (err) {
             console.error('[ERROR] Saving Microsoft user:', err.message);
-            res.redirect('/login-failed');
+            res.redirect('http://localhost:5173/dashboard');
         }
     }
 );
